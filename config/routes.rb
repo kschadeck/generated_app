f2e923b82aa9ec3dc5ec76b6e241ed47dc4cbba3
Kathleen Schadeck <kschadeck@gmail.com>
2014-04-15 11:55:57 -0500

    Finish demo app(generated app)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+#GeneratedApp::Application.routes.draw do  #Michael Hartl's way but did not work for db:migrate
+  resources :microposts
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
